b2398e1f8b0d208a458a8100a59e8c2cf95a4d33
Вадим Боровков <renegadeoffunk@yandex.ru>
2021-04-25 01:15:01 +0300

    finale

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("guru.springframework")
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
